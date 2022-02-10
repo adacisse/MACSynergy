c3e89c118baf3969da2938b262c77522b71b6384
mfaucillion <matthieu.faucillion@gmail.com>
2022-02-10 12:21:25 +0100

    Ajout class step transaction

--- Projet2Crowdfunding/Models/Step.cs
+++ Projet2Crowdfunding/Models/Step.cs
@@ -1,10 +1,20 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace Projet2Crowdfunding.Models
 {
     public class Step
     {
-        public Step()
-        {
-        }
+        public int Id { get; set; }
+
+        [Required]
+        public double Amount { get; set; }
+
+        [MaxLength(250)]
+        [Required]
+        public string Desccription { get; set; }
+
+        [Required]
+        public virtual Project Project { get; set; }
     }
 }
--- Projet2Crowdfunding/Models/Transaction.cs
+++ Projet2Crowdfunding/Models/Transaction.cs
@@ -1,8 +1,20 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace Projet2Crowdfunding.Models
 {
     public class Transaction
     {
-        
+        public int Id { get; set; }
+
+        [MaxLength(45)]
+        [Required]
+        public string Type { get; set; }
+
+        [Required]
+        public virtual Donation Donation { get; set; }
+
+        [Required]
+        public DateTime TransationDate { get; set; }
     }
 }
