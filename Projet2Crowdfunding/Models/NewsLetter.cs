a52cf97554ac132fb456d27a9fe3ab7244181302
HelyZika <sr.rchouk@gmail.com>
2022-02-10 14:12:30 +0100

    ajout NewsLetter

--- Projet2Crowdfunding/Models/NewsLetter.cs
+++ Projet2Crowdfunding/Models/NewsLetter.cs
@@ -1,8 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
+
 namespace Projet2Crowdfunding.Models
 {
     public class NewsLetter
     {
-        
+        public HashSet<Project> favorateProjets; //un groupe de projet favoris sans doublons
+        public HashSet<ProjectOwner> FavorateAssoPO; //un groupe d'associations favoris sans doublons
+        public HashSet<TypeCategory> FavorateCategory; 
     }
 }
