775528b76deca89cca41971fc92c2f970b6ac0c6
Cristina <cris_siempre2@hotmail.com>
2022-02-14 12:27:57 +0100

    correction sErviceaccount

--- Projet2Crowdfunding/Models/BddContext.cs
+++ Projet2Crowdfunding/Models/BddContext.cs
@@ -74,86 +74,128 @@ namespace Projet2Crowdfunding.Models
                 );
 
             //Instances Participant
-            //this.Participants.AddRange(
-            //new Participant
-            //{
-            //    Id = 1,
-            //    LastName = "Cisse",
-            //    FirstName = "Ada",
-            //    Gender = Gender.Female,
-            //    PhoneNumber = "6666666666",
-            //    Birthdate = DateTime.Parse("2013-07-04"),
-            //    Address = new Address
-            //    {
-            //        Id = 1,
-            //        StreetName = "Rue de la fleur",
-            //        StreetNumber = "12",
-            //        ZipCode = "57888",
-            //        City = "Toulouse",
-            //        Country = "France"
-            //    },
-            //    Account = new ParticipantAccount { Id = 1, Mail = "adacisse@gmail.com", Password = "aaaaa" },
-            //    User = new Account { Id = 1, Mail = "adacisse@gmail.com", Password = "aaaaa" }
-            //},
-            // new Participant
-            // {
-            //     Id = 2,
-            //     LastName = "Rchouk",
-            //     FirstName = "Sara",
-            //     Gender = Gender.Female,
-            //     PhoneNumber = "5555555555",
-            //     Birthdate = DateTime.Parse("2011-03-14"),
-            //     Address = new Address
-            //     {
-            //         Id = 2,
-            //         StreetName = "Rue du presidant",
-            //         StreetNumber = "24",
-            //         ZipCode = "75007",
-            //         City = "Paris",
-            //         Country = "France"
-            //     },
-            //     Account = new ParticipantAccount { Id = 2, Mail = "sararchouk@gmail.com", Password = "sssss" }
-            // },
-            // new Participant
-            // {
-            //     Id = 3,
-            //     LastName = "Equisoain",
-            //     FirstName = "Cristina",
-            //     Gender = Gender.Female,
-            //     PhoneNumber = "4444444444",
-            //     Birthdate = DateTime.Parse("1994-07-04"),
-            //     Address = new Address
-            //     {
-            //         Id = 3,
-            //         StreetName = "Calle de Barcelona",
-            //         StreetNumber = "52",
-            //         ZipCode = "36666",
-            //         City = "Madrid",
-            //         Country = "Espagne"
-            //     },
-            //     Account = new ParticipantAccount { Id = 3, Mail = "cristinaequisoain@gmail.com", Password = "ccccc" }
-            // },
-            // new Participant
-            // {
-            //     Id = 4,
-            //     LastName = "Faucillon",
-            //     FirstName = "Matthieu",
-            //     Gender = Gender.Male,
-            //     PhoneNumber = "3333333333",
-            //     Birthdate = DateTime.Parse("1990-07-04"),
-            //     Address = new Address
-            //     {
-            //         Id = 4,
-            //         StreetName = "Rue de la gare",
-            //         StreetNumber = "43",
-            //         ZipCode = "97532",
-            //         City = "Bordeaux",
-            //         Country = "France"
-            //     },
-            //     Account = new ParticipantAccount { Id = 4, Mail = "matthieufaucillon@gmail.com", Password = "mmmmm" }
-            // }
+            this.Participants.AddRange(
+            new Participant
+            {
+                Id = 1,
+                LastName = "Cisse",
+                FirstName = "Ada",
+                Gender = Gender.Female,
+                PhoneNumber = "6666666666",
+                Birthdate = DateTime.Parse("2013-07-04"),
+                Address = new Address
+                {
+                    Id = 1,
+                    StreetName = "Rue de la fleur",
+                    StreetNumber = "12",
+                    ZipCode = "57888",
+                    City = "Toulouse",
+                    Country = "France"
+                },
+                Account = new ParticipantAccount {
+                    Id = 1,
+                    Newsletter = false,
+                    ConfidentialityCharter = true,
+                    Account = new Account
+                    {
+                        Id = 1,
+                        Mail = "adacisse@gmail.com",
+                        Password = "aaaaa"
+                    }  
+                }
+            },
+             new Participant
+             {
+                 Id = 2,
+                 LastName = "Rchouk",
+                 FirstName = "Sara",
+                 Gender = Gender.Female,
+                 PhoneNumber = "5555555555",
+                 Birthdate = DateTime.Parse("2011-03-14"),
+                 Address = new Address
+                 {
+                     Id = 2,
+                     StreetName = "Rue du presidant",
+                     StreetNumber = "24",
+                     ZipCode = "75007",
+                     City = "Paris",
+                     Country = "France"
+                 },
+                  Account = new ParticipantAccount
+                  {
+                      Id = 2,
+                      Newsletter = true,
+                      ConfidentialityCharter = true,
+                      Account = new Account
+                      {
+                          Id = 2,
+                          Mail = "sararchouk@gmail.com",
+                          Password = "sssss"
+                      }
+                  }
+             },
+             new Participant
+             {
+                 Id = 3,
+                 LastName = "Equisoain",
+                 FirstName = "Cristina",
+                 Gender = Gender.Female,
+                 PhoneNumber = "4444444444",
+                 Birthdate = DateTime.Parse("1994-07-04"),
+                 Address = new Address
+                 {
+                     Id = 3,
+                     StreetName = "Calle de Barcelona",
+                     StreetNumber = "52",
+                     ZipCode = "36666",
+                     City = "Madrid",
+                     Country = "Espagne"
+                 },
+                   Account = new ParticipantAccount
+                   {
+                       Id = 3,
+                       Newsletter = true,
+                       ConfidentialityCharter = true,
+                       Account = new Account
+                       {
+                           Id = 3,
+                           Mail = "cristinaequisoain@gmail.com",
+                           Password = "ccccc"
+                       }
+                   }
+             },
+             new Participant
+             {
+                 Id = 4,
+                 LastName = "Faucillon",
+                 FirstName = "Matthieu",
+                 Gender = Gender.Male,
+                 PhoneNumber = "3333333333",
+                 Birthdate = DateTime.Parse("1990-07-04"),
+                 Address = new Address
+                 {
+                     Id = 4,
+                     StreetName = "Rue de la gare",
+                     StreetNumber = "43",
+                     ZipCode = "97532",
+                     City = "Bordeaux",
+                     Country = "France"
+                 },
+                  Account = new ParticipantAccount
+                  {
+                      Id = 4,
+                      Newsletter = false,
+                      ConfidentialityCharter = true,
+                      Account = new Account
+                      {
+                          Id = 4,
+                          Mail = "matthieufaucillon@gmail.com",
+                          Password = "mmmmm"
+                      }
+                  }
+             }
 
-            //);
+            );
 
             this.SaveChanges();
         }
