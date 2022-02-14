7f6659dce3577fd456f803a74af550ed2e5934dc
adacisse <adacisse90@gmail.com>
2022-02-14 01:15:35 +0100

    Ajout ServiceLogin(creation participant, pp, gp et debut authentification.

---
+++ Projet2Crowdfunding/Controllers/UserController.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Projet2Crowdfunding.Models;
+
+namespace Projet2Crowdfunding.Controllers
+{
+    public class UserController : Controller
+    {
+        private BddContext bddContext;
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost, ActionName("Login")]
+
+        public IActionResult UserLogin(string Mail, string Password)
+        {
+            
+            bddContext = new BddContext();
+            bddContext.Users.FindAsync();
+
+            return View();
+        }
+    }
+}
