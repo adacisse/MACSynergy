a057eeb727761089369ca5b317b07bf0e88a4070
adacisse <adacisse90@gmail.com>
2022-02-19 16:50:55 +0100

    Contribution sur le site via l'API de Stripe ok

---
+++ Projet2Crowdfunding/Controllers/PaymentsController.cs
@@ -0,0 +1,75 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Projet2Crowdfunding.Service;
+using Projet2Crowdfunding.ViewModels;
+using Stripe;
+using Stripe.Checkout;
+
+namespace Projet2Crowdfunding.Controllers
+{
+    public class PaymentsController : Controller
+    {
+
+        public PaymentsController()
+        {
+            StripeConfiguration.ApiKey = "sk_test_51KUbEVFZdOzBn1VnNLVh8lPPu6fTUDUrkMOS7lOremlO7jGBkZg54ArHR0wd3tRAQTtfYmRUGZ0xJmSMkF0RX6iV00nrUzdcIC";
+        }
+        //public IActionResult ParticipantPayment()
+        //{
+        //    PaymentViewModel viewModel = new PaymentViewModel { Authentify = HttpContext.User.Identity.IsAuthenticated }; //cookies
+        //    if (HttpContext.User.Identity.IsAuthenticated)
+        //    {
+                
+        //    }
+        //    return View(P);
+        //}
+
+        [HttpPost]
+        public IActionResult CreateCheckoutSession(PaymentViewModel viewModel) 
+
+        {
+            var options = new SessionCreateOptions
+            {
+                LineItems = new List<SessionLineItemOptions>
+        {
+          new SessionLineItemOptions
+          {
+            PriceData = new SessionLineItemPriceDataOptions
+            {
+
+            UnitAmount = (long?)(viewModel.Amount*100),
+              Currency = "eur",
+              ProductData = new SessionLineItemPriceDataProductDataOptions
+              {
+                Name = "Contribution",
+              },
+
+            },
+            Quantity = 1,
+          },
+        },
+                Mode = "payment",
+                SuccessUrl = "http://localhost:5000/Payments/PaymentSuccess",
+                CancelUrl = "http://localhost:5000/Home/Index",
+            };
+            
+            var service = new SessionService();
+            Session session = service.Create(options);
+
+            Response.Headers.Add("Location", session.Url);
+            return new StatusCodeResult(303);
+        }
+
+        public IActionResult PaymentPage()
+        {
+            return View();
+        }
+        public IActionResult PaymentSuccess()
+        {
+            return View();
+        }
+    }
+}
---
+++ Projet2Crowdfunding/ViewModels/PaymentViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Projet2Crowdfunding.Models;
+
+namespace Projet2Crowdfunding.ViewModels
+{
+    public class PaymentViewModel
+    {
+        internal bool Authentify;
+
+        public double Amount { get; set; }
+        public Donation Donation { get; set; }
+        public Account Account { get; set; }
+        public Participant Participant { get; set; }
+    }
+}
